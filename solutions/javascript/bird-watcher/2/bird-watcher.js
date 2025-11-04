077b2df8e34975cc94c60b60557f8c0fa953c31c
Ahmad Fadli <thancraft313@gmail.com>
2025-11-04 19:40:39 +0900

    Update bird-watcher.js

--- solutions/javascript/bird-watcher/2/bird-watcher.js
+++ solutions/javascript/bird-watcher/2/bird-watcher.js
@@ -1,19 +1,26 @@
 export function totalBirdCount(birdsPerDay) {
   var terhitung = 0;
+  
   for (let i = 0; i < birdsPerDay.length; i++) {
         terhitung += birdsPerDay[i];
   }
   return terhitung
 }
+
+
 export function birdsInWeek(birdsPerDay, week) {
   var total = 0;
+  
   for (let i = 7 * (week - 1); i <7 * week; i++) {
         total += birdsPerDay[i];
   }
   return total
 }
+
+
 export function fixBirdCountLog(birdsPerDay) {
   var total = 0;
+  
   for (let i = 0; i < birdsPerDay.length ; i += 2) {
         birdsPerDay[i] += 1;
   }
